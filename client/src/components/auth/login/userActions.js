ebbe9cc198ef4f1b0ba6929a5c0f875c360a052e
Shanna <shannameow@gmail.com>
2021-08-31 22:22:53 -0400

    added new domain to localhost

--- client/src/components/auth/login/userActions.js
+++ client/src/components/auth/login/userActions.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 export const getUser = (_id) => async (dispatch) => {
-  const res = await axios.get("http://localhost:5000/api/auth/loggedIn");
+  const res = await axios.get(
+    "https://astralspace.herokuapp.com/api/auth/loggedIn"
+  );
   dispatch({
     type: getUser,
     payload: res.data.data,
