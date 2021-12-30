5c75e740f6fd78ea6a1a71815daafc71bbec4241
Shanna <shannameow@gmail.com>
2021-12-29 21:34:24 -0500

    first commit

--- config/keys.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  mongoURI: "YOUR_MONGO_URI_HERE",
-  JWT_SECRET: "eWtYuyI56OpKlMnhfFswazxcRkwemdncidsoiEREFDDCcvdD",
-  refreshTokenSecret: "this_SomeSeCretive-sh17-RI8t_huRr",
-  tokenLife: 36000,
-  refreshTokenLife: 86400,
-  EMAIL: "shannabreaking@gmail.com",
-  PASS: "StOpHacKerz!2k24",
-};
